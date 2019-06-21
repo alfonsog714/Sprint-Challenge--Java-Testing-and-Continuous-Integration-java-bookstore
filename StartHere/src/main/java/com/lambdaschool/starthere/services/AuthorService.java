91299d982966c0489a29e7f919c53fadd62e8597
Alfonso <alfonso.garcia2324@gmail.com>
2019-06-21 09:04:29 -0700

    Added services for book and author

---
+++ StartHere/src/main/java/com/lambdaschool/starthere/services/AuthorService.java
@@ -0,0 +1,18 @@
+package com.lambdaschool.starthere.services;
+
+import com.lambdaschool.starthere.models.Author;
+
+import java.util.List;
+
+public interface AuthorService
+{
+    List<Author> findAll();
+
+    Author findAuthorById(long id);
+
+    void delete(long id);
+
+    Author save(Author author);
+
+    Author update(Author author, long id);
+}
